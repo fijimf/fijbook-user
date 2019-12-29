3c15d39a309dd8e4452b430fa441d2ce256d94fd
Jim Frohnhofer <jfrohnhofer@kabbage.com>
2019-12-29 02:35:39 -0500

    tests

--- src/test/scala/com/fijimf/deepfoj/user/model/AuthTokenSpec.scala
+++ src/test/scala/com/fijimf/deepfoj/user/model/AuthTokenSpec.scala
@@ -47,4 +47,54 @@ class AuthTokenSpec extends FunSpec {
     }
 
   }
+  describe("An option of authToken") {
+    val some: Option[AuthToken] = Some(AuthToken(UUID.randomUUID(), UUID.randomUUID(), new DateTime()))
+    val none: Option[AuthToken] = Option.empty[AuthToken]
+    it(" can generate JSON") {
+      val js: String = some.asJson.spaces2
+      println(js)
+      val ks: String = none.asJson.spaces2
+      println(ks)
+    }
+
+    it("can parse JSON ") {
+      val js: String =
+        """{
+          |  "id" : "e2405681-cecd-4284-96be-372d0c35d908",
+          |  "userId" : "a6cf644b-fa9f-498a-8049-42493c9426d1",
+          |  "expiry" : "2019-12-29 01:24:25.308"
+          |}
+          |""".stripMargin
+      decode[Option[AuthToken]](js) match {
+        case Left(thr) => fail(thr)
+        case Right(Some(authToken)) =>
+          assert(authToken.id.toString === "e2405681-cecd-4284-96be-372d0c35d908")
+          assert(authToken.userId.toString === "a6cf644b-fa9f-498a-8049-42493c9426d1")
+          assert(authToken.expiry === DateTime.parse("2019-12-29 01:24:25.308", DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS")))
+        case Right(_) => fail()
+      }
+      val ks: String = """null"""
+      decode[Option[AuthToken]](ks) match {
+        case Left(thr) => fail(thr)
+        case Right(None) => //OK
+        case Right(_) => fail()
+      }
+
+    }
+
+
+    it("can round trip JSON idempotently") {
+      decode[Option[AuthToken]](some.asJson.spaces2) match {
+        case Left(thr) => fail(thr)
+        case Right(z) =>
+          assert(some === z)
+      }
+      decode[Option[AuthToken]](none.asJson.spaces2) match {
+        case Left(thr) => fail(thr)
+        case Right(z) =>
+          assert(none === z)
+      }
+    }
+
+  }
 }
